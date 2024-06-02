52c7978615d1ca9262a1160b71ad68cdf61388c8
Georgy <gzagayko@gmail.com>
2024-06-02 18:27:54 +0300

    feat: added chatId to message entity

--- src/chat/entities/chatMessage.ts
+++ src/chat/entities/chatMessage.ts
@@ -1,6 +1,12 @@
 import { BaseEntity } from '@/common/base-entity';
 import { User } from '@/users/entities/user.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+} from 'typeorm';
 import { Chat } from '@/chat/entities/chat';
 
 @Entity('chat_messages')
@@ -9,7 +15,11 @@ export class ChatMessage extends BaseEntity {
   id: string;
 
   @ManyToOne(() => Chat, (chat) => chat.chatMessages)
+  @JoinColumn({ name: 'chatId' })
   chat: Chat;
+
+  @Column({ nullable: false })
+  chatId: string;
 
   @ManyToOne(() => User, (user) => user.chatMessages, { eager: true })
   user: User;
